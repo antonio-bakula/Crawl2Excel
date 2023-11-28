e4fccd886e7be16a66edcfd34d24c3adcd2747d2
Antonio Bakula <antonio@wem.hr>
2023-11-28 23:24:43 +0100

    Add error and problems logging #7  Error 503 #6

---
+++ Crawl2Excel.Engine/Code/Crawl2ExcelLogger.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crawl2Excel.Engine.Code
+{
+	public static class Crawl2ExcelLogger
+	{
+		public static ILogger Logger { get; set; }
+	}
+}
--- Crawl2Excel/Program.cs
+++ Crawl2Excel/Program.cs
@@ -1,6 +1,9 @@
 ﻿using Crawl2Excel;
 using Crawl2Excel.Engine;
 using Crawl2Excel.Engine.Code;
+using Karambolo.Extensions.Logging.File;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using System.Diagnostics;
 
 var sw = new Stopwatch();
@@ -27,12 +30,42 @@ if (!parameters.ParametersValid)
 	return 0;
 }
 
+var services = new ServiceCollection();
+
+services.AddLogging(builder =>
+{
+	builder.AddFile(o => 
+	{
+		o.RootPath = AppContext.BaseDirectory;
+		o.FileAccessMode = LogFileAccessMode.OpenTemporarily;
+		o.FileEncoding = System.Text.Encoding.UTF8;
+		o.DateFormat = "dd.MM.yyyy HH:mm:ss";
+		o.CounterFormat = "000";
+		o.MaxFileSize = 1024 * 1024 * 10;
+		o.Files = new[]
+		{
+			new LogFileOptions
+			{
+				Path = "Crawl2Excel-<counter>.log",
+			}
+		};
+	});
+});
+
+
+var sp = services.BuildServiceProvider();
+var loggerFactory = sp.GetService<ILoggerFactory>();
+Crawl2ExcelLogger.Logger = loggerFactory.CreateLogger("Crawl2Excel");
+
+Crawl2ExcelLogger.Logger.LogInformation($"Crawl2Excel started. Start Url: {parameters.CrawlStartUrl}");
+
 var crawler = new Crawler(parameters.CrawlStartUrl, parameters.ExcelFileName);
 crawler.Options.OverwriteResultExcelIfExists = parameters.Switches.Contains("owr");
 crawler.Options.LaunchExcelWhenFinished = parameters.Switches.Contains("le");
 await crawler.Start();
 sw.Stop();
 Console.WriteLine(sw.Elapsed);
+sp.Dispose();
 return 1;
 
 
